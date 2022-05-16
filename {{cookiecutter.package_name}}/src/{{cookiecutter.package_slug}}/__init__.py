9d156167ed4ecd4f2655ea4e183dcfcdc7bca84b
Robin Kennedy-Reid <rlkennedyreid@carneddau.cymru>
2022-05-16 21:37:14 +0100

    🧑‍💻 Use 'official' way of versioning poetry package
    Ensures a single source of truth for the versioning

--- {{cookiecutter.package_name}}/src/{{cookiecutter.package_slug}}/__init__.py
+++ {{cookiecutter.package_name}}/src/{{cookiecutter.package_slug}}/__init__.py
@@ -1,1 +1,5 @@
-__version__ = "{{cookiecutter.version}}"
+from importlib.metadata import version
+
+# See: https://github.com/python-poetry/poetry/pull/2366#issuecomment-652418094
+# For info on poetry versionion for Python < 3.8
+__version__ = version(__name__)
--- {{cookiecutter.package_name}}/tests/test_{{cookiecutter.package_slug}}.py
+++ {{cookiecutter.package_name}}/tests/test_{{cookiecutter.package_slug}}.py
@@ -2,4 +2,7 @@ from {{cookiecutter.package_slug}} import __version__
 
 
 def test_version():
+    """A useless test.
+    Only present to make sure the testing library is working.
+    """
     assert __version__ == "{{cookiecutter.version}}"
